72d6a449d10d0501d5c603cb36fdae1a8e4cf7e0
RenatoConfuorto <renato.confuorto@outlook.it>
2022-06-28 06:01:17 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,44 +1,54 @@
   <nav>
-    <ul>
-      <li>
-        <a href="#">characters</a>
-      </li>
-      
-      <li>
-        <a href="#">comics</a>
-      </li>
-      
-      <li>
-        <a href="#">movies</a>
-      </li>
-      
-      <li>
-        <a href="#">tv</a>
-      </li>
-      
-      <li>
-        <a href="#">games</a>
-      </li>
-      
-      <li>
-        <a href="#">collectibles</a>
-      </li>
-      
-      <li>
-        <a href="#">videos</a>
-      </li>
-      
-      <li>
-        <a href="#">fans</a>
-      </li>
-      
-      <li>
-        <a href="#">news</a>
-      </li>
-      
-      <li>
-        <a href="#">shop</a>
-      </li>
-      
-    </ul>
+    <div class="logo">
+      <img src="{{ asset('images/dc-logo.png') }}" alt="">
+    </div>
+
+    <div class="navbar">
+      <ul class="navbar-list">
+        <li>
+          <a href="#">characters</a>
+        </li>
+  
+        <li>
+          <a href="#">comics</a>
+        </li>
+  
+        <li>
+          <a href="#">movies</a>
+        </li>
+  
+        <li>
+          <a href="#">tv</a>
+        </li>
+  
+        <li>
+          <a href="#">games</a>
+        </li>
+  
+        <li>
+          <a href="#">collectibles</a>
+        </li>
+  
+        <li>
+          <a href="#">videos</a>
+        </li>
+  
+        <li>
+          <a href="#">fans</a>
+        </li>
+  
+        <li>
+          <a href="#">news</a>
+        </li>
+  
+        <li>
+          <a href="#">shop</a>
+        </li>
+  
+      </ul>
+    </div>
+
+    <div class="search-bar">
+      Search
+    </div>
   </nav>
