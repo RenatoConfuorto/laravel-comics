c16afcd585af54956b9623d707e87a11ae604687
RenatoConfuorto <renato.confuorto@outlook.it>
2022-06-28 06:16:13 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,80 @@
-  Footer
+<div class="footer-top">
+
+  <div class="footer-list-wrapper">
+    <div class="footer-col">
+      <div class="footer-list">
+        <h3>footer-list</h3>
+        <ul>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div>
+  
+      <div class="footer-list">
+        <h3>footer-list</h3>
+        <ul>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+        </ul>
+      </div>
+    </div>
+  
+    <div class="footer-col">
+      <div class="footer-list">
+        <h3>footer-list</h3>
+        <ul>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+        </ul>
+      </div>
+    </div>
+  
+    <div class="footer-col">
+      <div class="footer-list">
+        <h3>footer-list</h3>
+        <ul>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-rights">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab quas culpa quasi reiciendis similique odit laborum neque dolorem iste maxime natus distinctio harum dolores, saepe quos, provident consectetur aspernatur incidunt.</p>
+  </div>
+</div>
+
+<div class="footer-bottom">
+  <div class="cta-button">
+    <a href="#">sign ip now</a>
+  </div>
+
+  <div class="socials">
+    <h3>follow us</h3>
+    <ul>
+      <li><a href="#">icon</a></li>
+      <li><a href="#">icon</a></li>
+      <li><a href="#">icon</a></li>
+      <li><a href="#">icon</a></li>
+      <li><a href="#">icon</a></li>
+    </ul>
+  </div>
+</div>
