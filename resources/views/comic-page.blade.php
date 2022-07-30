13f65bb94108f1c04d22c6e5118eed02f0745a96
RenatoConfuorto <renato.confuorto@outlook.it>
2022-07-30 19:00:57 +0200

    agiiustamenti layout

--- resources/views/comic-page.blade.php
+++ resources/views/comic-page.blade.php
@@ -5,59 +5,73 @@
 @endsection
 
 @section('main-content')
-    <div class="comic-cover">
-        <img src="{{ $thumb }}" alt="">
-    </div>
+    <div class="stripe"></div>
+    <div class="comic-container">
+      <div class="container">
 
-    <div class="comic-description">
-        <h2>{{ $title }}</h2>
-        <div class="price-section">
-            <h3>U.S. price {{ $price }}</h3>
-            <p>Available</p>
-            <p>
-                check availability
-            </p>
-        </div>
-        <p>{!! $description !!}</p>
-    </div>
-
-    <div class="comic-details">
-      <div class="col">
-        <h3>talent</h3>
-        <div class="col-section">
-          <h4>Art By:</h4>
-          <div class="col-section-content">
-            {{ collect($artists)->implode(', ') }}
-          </div>
+        <div class="comic-cover">
+          <span>comic book</span>
+          <img src="{{ $thumb }}" alt="">
+          <span>view gallery</span>
         </div>
 
-        <div class="col-section">
-          <h4>Written By:</h4>
-          <div class="col-section-content">
-            {{  collect($writers)->implode(', ') }}
-          </div>
+        <div class="comic-description">
+            <h2>{{ $title }}</h2>
+            <div class="price-section">
+                <h3>U.S. price <span>{{ $price }}</span></h3>
+                <div class="available">
+                  <p>Available</p>
+                  <p>
+                      check availability
+                  </p>
+                </div>
+            </div>
+            <p class="description">{!! $description !!}</p>
         </div>
+
       </div>
 
-      <div class="col">
-        <h3>specs</h3>
-        <div class="col-section">
-          <h4>Series:</h4>
-          <div class="col-section-content">
-            {{ $series }}
+      <div class="comic-details">
+        <div class="container">
+          <div class="col">
+              <h3>talent</h3>
+              <div class="col-section">
+                  <h4>Art By:</h4>
+                  <div class="col-section-content light">
+                      {{ collect($artists)->implode(', ') }}
+                  </div>
+              </div>
+
+              <div class="col-section">
+                  <h4>Written By:</h4>
+                  <div class="col-section-content light">
+                      {{ collect($writers)->implode(', ') }}
+                  </div>
+              </div>
           </div>
-        </div>
-        <div class="col-section">
-          <h4>U.S. Price:</h4>
-          <div class="col-section-content">
-            {{ $price }}
+
+          <div class="col">
+              <h3>specs</h3>
+              <div class="col-section">
+                  <h4>Series:</h4>
+                  <div class="col-section-content">
+                      {{ $series }}
+                  </div>
+              </div>
+              <div class="col-section">
+                  <h4>U.S. Price:</h4>
+                  <div class="col-section-content">
+                      {{ $price }}
+                  </div>
+              </div>
+              <div class="col-section">
+                  <h4>On Sale Date:</h4>
+                  <div class="col-section-content">
+                      {{ $sale_date }}
+                  </div>
+              </div>
           </div>
-        </div>
-        <div class="col-section">
-          <h4>On Sale Date:</h4>
-          <div class="col-section-content">
-            {{ $sale_date }}
-          </div>
+
         </div>
       </div>
     </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,80 +1,142 @@
-<div class="footer-top">
+<div class="icons-container">
+  <ul>
+    <li>
+      <a href="#">
+        <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="#">
+        <span>Digigtal Comics</span>
+      </a>
+    </li>
 
-  <div class="footer-list-wrapper">
-    <div class="footer-col">
-      <div class="footer-list">
-        <h3>footer-list</h3>
-        <ul>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-        </ul>
-      </div>
-  
-      <div class="footer-list">
-        <h3>footer-list</h3>
-        <ul>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-        </ul>
-      </div>
-    </div>
-  
-    <div class="footer-col">
-      <div class="footer-list">
-        <h3>footer-list</h3>
-        <ul>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-        </ul>
-      </div>
-    </div>
-  
-    <div class="footer-col">
-      <div class="footer-list">
-        <h3>footer-list</h3>
-        <ul>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-          <li><a href="#">link</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+    <li>
+      <a href="#">
+        <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="#">
+        <span>Dc Merchandise</span>
+      </a>
+    </li>
 
-  <div class="footer-rights">
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab quas culpa quasi reiciendis similique odit laborum neque dolorem iste maxime natus distinctio harum dolores, saepe quos, provident consectetur aspernatur incidunt.</p>
+    <li>
+      <a href="#">
+        <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="#">
+        <span>Subscription</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="#">
+        <span>Comic shop locator</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="#">
+        <span>Dc Power Visa</span>
+      </a>
+    </li>
+
+  </ul>
+</div>
+
+<div class="footer">
+  <div class="container">
+    <div class="footer-list">
+
+      <div class="col-3">
+        <h3>Dc Comics</h3>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Tv</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+
+
+        <h3>Shop</h3>
+        <ul>
+          <li><a href="#">Shop Dc</a></li>
+          <li><a href="#">Shop Dc Collectibles</a></li>
+        </ul>
+      </div>
+
+      <div class="col-3">
+        <h3>Dc</h3>
+        <ul>
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+
+      </div>
+
+      <div class="col-3">
+        <h3>Sites</h3>
+        <ul>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazines</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+
+        </ul>
+      </div>
+
+      
+    </div>
   </div>
 </div>
 
-<div class="footer-bottom">
-  <div class="cta-button">
-    <a href="#">sign ip now</a>
-  </div>
+<div class="footer-action">
+  <div class="container">
+    <div class="sign-up-section">
+      <a href="#">Sign-up Now!</a>
+    </div>
 
-  <div class="socials">
-    <h3>follow us</h3>
-    <ul>
-      <li><a href="#">icon</a></li>
-      <li><a href="#">icon</a></li>
-      <li><a href="#">icon</a></li>
-      <li><a href="#">icon</a></li>
-      <li><a href="#">icon</a></li>
-    </ul>
+    <div class="socials">
+      <h3>Follow us</h3>
+      <ul>
+        <li>
+          <a href="#">
+            <img src="{{ asset('images/footer-facebook.png') }}" alt="#">
+          </a>
+        </li>
+        
+        <li>
+          <a href="#">
+            <img src="{{ asset('images/footer-twitter.png') }}" alt="#">
+          </a>
+        </li>
+        
+        <li>
+          <a href="#">
+            <img src="{{ asset('images/footer-youtube.png') }}" alt="#">
+          </a>
+        </li>
+        
+        <li>
+          <a href="#">
+            <img src="{{ asset('images/footer-pinterest.png') }}" alt="#">
+          </a>
+        </li>
+        
+        <li>
+          <a href="#">
+            <img src="{{ asset('images/footer-periscope.png') }}" alt="#">
+          </a>
+        </li>
+        
+      </ul>
+    </div>
   </div>
 </div>
